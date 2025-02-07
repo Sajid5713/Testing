eb2e1a00ada35f245490e8e943c5676897fb53f0
Sajig5713 <sajid3115936@gmail.com>
2025-02-07 21:50:44 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,52 @@
+const heading = React.createElement(
+    'div',
+    { 
+        id:'parent',
+    },
+    [
+        React.createElement(
+            'div',
+            {
+                id:'child'
+            },
+            [
+                React.createElement(
+                    'h1',
+                    {className:'h1'},
+                    'I am an H1 tag'
+                ),
+                React.createElement(
+                    'h2',
+                    {},
+                    'I am an H2 tag'
+                )
+            ],
+            
+        ),
+        React.createElement(
+            'div',
+            {
+                id:'child2'
+            },
+            [
+                React.createElement(
+                    'h1',
+                    {className:'h1'},
+                    'I am an H1 tag'
+                ),
+                React.createElement(
+                    'h2',
+                    {},
+                    'I am an H2 tag'
+                )
+            ],
+            
+        )
+    ]
+)
+
+console.log(heading)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
